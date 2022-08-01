5d7431470742976aa457177e6dc11702e2a93399
sermeor <sergiomenaortega95@gmail.com>
2022-08-01 19:43:07 +0100

    Images

--- main.php
+++ main.php
@@ -103,7 +103,7 @@ html,body,h1,h2,h3,h4 {font-family:"Lato", sans-serif}
 
     <div class="w3-third w3-margin-bottom">
       <div class="w3-card-4">
-        <img src="/w3images/bill_example.jpg" alt="John" style="width:100%">
+        <img src="images/bill_example.jpg" alt="John" style="width:100%">
         <div class="w3-container">
           <h3>BILL LEVKOFF</h3>
           <p class="w3-opacity">Bridesmaids & special occasion</p>
