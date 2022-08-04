866b750ad9de5d4585ba1b3d874531bfb2f89ada
sermeor <sergiomenaortega95@gmail.com>
2022-08-04 18:36:06 +0100

    Changes

--- main.php
+++ main.php
@@ -60,10 +60,10 @@ a {text-decoration: none;}
 <div class="w3-half w3-container">
 
   <div class="w3-panel" id="cycler">
-    <img class="active" src="images/1.png" style="width:100%"/>
-    <img src="images/2.png" style="width:100%" />
-    <img src="images/3.png"style="width:100%" />
-    <img src="images/4.png"style="width:100%" />
+    <img class="active" src="images/1.png" style="width:50%"/>
+    <img src="images/2.png" style="width:50%" />
+    <img src="images/3.png"style="width:50%" />
+    <img src="images/4.png"style="width:50%" />
   </div>
 
 </div>
